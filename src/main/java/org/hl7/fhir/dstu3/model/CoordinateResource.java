4f9fc5edd2c106fca2350cf7b4d0705207967853
Charlie Marcus <charlie.marcus1@nhs.net>
2019-12-12 12:05:38 +0000

    replace old extensions with coordinate resource

---
+++ src/main/java/org/hl7/fhir/dstu3/model/CoordinateResource.java
@@ -0,0 +1,45 @@
+package org.hl7.fhir.dstu3.model;
+
+import ca.uhn.fhir.context.FhirVersionEnum;
+import ca.uhn.fhir.model.api.annotation.Child;
+import ca.uhn.fhir.model.api.annotation.ResourceDef;
+import ca.uhn.fhir.util.ElementUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+@ResourceDef(name = "CoordinateResource")
+@Getter
+@Setter
+public class CoordinateResource extends DomainResource {
+
+  @Child(name = "xCoordinate", min = 1, order = 0)
+  private IntegerType xCoordinate;
+
+  @Child(name = "yCoordinate", min = 1, order = 1)
+  private IntegerType yCoordinate;
+
+  @Override
+  public DomainResource copy() {
+    CoordinateResource copy = new CoordinateResource();
+    super.copyValues(copy);
+    copy.xCoordinate = xCoordinate;
+    copy.yCoordinate = yCoordinate;
+    return copy;
+  }
+
+  @Override
+  public ResourceType getResourceType() {
+    return null;
+  }
+
+  @Override
+  public FhirVersionEnum getStructureFhirVersionEnum() {
+    return FhirVersionEnum.DSTU3;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return ElementUtil.isEmpty(xCoordinate, yCoordinate);
+  }
+
+}
